8f7cc1c1e976e53cedcbf610d0a3326aa6c7b75d
zakdavlin <105325923+zakdavlin@users.noreply.github.com>
2023-02-04 21:52:04 -0600

    Updated with BBQ code

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Streaming-05-smart-smoker
+bbq_producer is a producer that is streaming
+smoker temperatures for the smoker and 2 different foods.
+Each temperature is sent to a different queue and picked 
+up by that queue's listener.
+Author: Zak Davlin
+
+General Design Questions
+
+How many producers processes do you need to read the temperatures: We need only one producer 
+How many queues do we use: 3 Queues, one temperatrue per queue.
+How many listening callback functions do we need (Hint: one per queue): 3 Listenting functions
+
+Requirements to run
+
+RabbitMQ server running
+pika installed in your active environment
+
+You can change the host in the code
+if you set show_offer to true it will bring up rabbitmq admin page. It is currently set to false.
+Notice in my screenshot below I used a 1 second timer just for testing. I changed it to 30 seconds after I got my screenshot!
+Screenshot!
+![Davlin working messages!](Screenshot.JPG)
---
+++ bbq_producer.py
@@ -0,0 +1,84 @@
+"""bbq_producer is a producer that is streaming
+smoker temperatures for the smoker and 2 different foods.
+Each temperature is sent to a different queue and picked 
+up by that queue's listener.
+Author: Zak Davlin """
+#Import needed Modules
+import pika
+import sys
+import webbrowser
+import csv
+import time
+#Show Rabbit MQ admin page if show_offer is True
+def offer_rabbitmq_admin_site(show_offer):
+    """Offer to open the RabbitMQ Admin website"""
+    if show_offer==True:
+        ans = input("Would you like to monitor RabbitMQ queues? y or n ")
+        print()
+        if ans.lower() == "y":
+            webbrowser.open_new("http://localhost:15672/#/queues")
+            print()
+
+def send_message(host: str, queue_name: str, message: str):
+    """
+    Creates and sends a message to the queue each execution.
+    This process runs and finishes.
+
+    Parameters:
+        host (str): the host name or IP address of the RabbitMQ server
+        queue_name (str): the name of the queue
+        message (str): the message to be sent to the queue
+    """
+
+    try:
+        # create a blocking connection to the RabbitMQ server
+        conn = pika.BlockingConnection(pika.ConnectionParameters(host))
+        # use the connection to create a communication channel
+        ch = conn.channel()
+        # use the channel to declare a durable queue
+        # a durable queue will survive a RabbitMQ server restart
+        # and help ensure messages are processed in order
+        # messages will not be deleted until the consumer acknowledges
+        ch.queue_declare(queue=queue_name, durable=True)
+        # use the channel to publish a message to the queue
+        # every message passes through an exchange
+        ch.basic_publish(exchange="", routing_key=queue_name, body=message)
+        # print a message to the console for the user
+        print(f" [x] Sent {message}")
+    except pika.exceptions.AMQPConnectionError as e:
+        print(f"Error: Connection to RabbitMQ server failed: {e}")
+        sys.exit(1)
+    finally:
+        # close the connection to the server
+        conn.close()
+
+""" Create csv function to read from file and turn it into a message"""
+#opens smoker File
+with open("smoker-temps.csv",'r') as file:
+    reader=csv.reader(file,delimiter=",")
+#read in rows 
+    for row in reader:
+     #Read just the timestamp and store it
+        fstringtime=f"{row[0]}"
+        #Read smoker reading and store it
+        smokertemp=f"{row[1]}"
+        #Read Food A reading and store it
+        foodA=f"{row[2]}"
+        #Read Food B reading and store it
+        foodB=f"{row[3]}"
+        #Set up messages 
+        smoker_message=f"{fstringtime},{smokertemp}"
+        foodA_message=f"{fstringtime},{foodA}"
+        foodB_message=f"{fstringtime},{foodB}"
+        #Declare a host name
+        host="localhost"
+        #Get those messages rolling out!
+        send_message(host,"01-smoker",smoker_message)
+        send_message(host,"02-food-A",foodA_message)
+        send_message(host,"03-food-B",foodB_message)
+        #Slow down the output to one message every 30 seconds
+        time.sleep(30)
+
+
+
+
